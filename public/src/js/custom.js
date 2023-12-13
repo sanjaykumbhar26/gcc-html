ba3bd9e7a4d280f6085f95e9bafd6b43fe86454e
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2023-12-13 09:35:01 +0530

    archive page readmore option added

--- public/src/js/custom.js
+++ public/src/js/custom.js
@@ -303,3 +303,30 @@ $(".toggle-password").click(function() {
 });
 
 
+///
+$(document).ready(function(){
+     function AddReadMore() {
+      var carLmt = 100;
+      var readMoreTxt = " ...read more";
+      var readLessTxt = " read less";
+      $(".add-read-more").each(function () {
+         if ($(this).find(".first-section").length)
+            return;
+         var allstr = $(this).text();
+         if (allstr.length > carLmt) {
+            var firstSet = allstr.substring(0, carLmt);
+            var secdHalf = allstr.substring(carLmt, allstr.length);
+            var strtoadd = firstSet + "<span class='second-section'>" + secdHalf + "</span><span class='read-more'  title='Click to Show More'>" + readMoreTxt + "</span><span class='read-less' title='Click to Show Less'>" + readLessTxt + "</span>";
+            $(this).html(strtoadd);
+         }
+      });
+      $(document).on("click", ".read-more,.read-less", function () {
+         $(this).closest(".add-read-more").toggleClass("show-less-content show-more-content");
+      });
+   }
+
+   AddReadMore();
+});
+
+
+
