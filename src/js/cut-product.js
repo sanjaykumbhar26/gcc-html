017d30e135e0aa2dbd648546bbd9a7b66722aea2
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2024-01-22 21:02:38 +0530

    update code

--- src/js/cut-product.js
+++ src/js/cut-product.js
@@ -249,6 +249,21 @@ jQuery(document).ready(function () {
   AddReadMore();
 });
 
+jQuery(document).ready(function () {
+  jQuery(".show-more").click(function () {
+    if (jQuery(".full-dec").is(":visible")) {
+      jQuery(".short").show();
+      jQuery(".full-dec").hide();
+      jQuery(".show-more").text("read more");
+    } else {
+      jQuery(".short").hide();
+      jQuery(".full-dec").show();
+      jQuery(".show-more").text("read less");
+    }
+  });
+});
+
+
 // rating
 let selectedRating = 0;
 function handleRating(rating) {
