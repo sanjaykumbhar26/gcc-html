acefeef24b95be08dbc8cb7f30ee5f2ed4c88326
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2024-01-23 18:28:25 +0530

    update code

--- src/js/cut-page.js
+++ src/js/cut-page.js
@@ -36,51 +36,10 @@ window.onload = function () {
 
 
 // Read More
-jQuery(document).ready(function () {
-  function AddReadMore() {
-    var carLmt = 470;
-    var readMoreTxt = " ...read more";
-    var readLessTxt = " read less";
-    jQuery(".add-read-more .term-description").each(function () {
-      if ($(this).find(".term-description").length) return;
-      var allstr = $(this).text();
-      if (allstr.length > carLmt) {
-        var firstSet = allstr.substring(0, carLmt);
-        var secdHalf = allstr.substring(carLmt, allstr.length);
-        var strtoadd =
-          firstSet +
-          "<span class='second-section'>" +
-          secdHalf +
-          "</span><span class='read-more'  title='Click to Show More'>" +
-          readMoreTxt +
-          "</span><span class='read-less' title='Click to Show Less'>" +
-          readLessTxt +
-          "</span>";
-        jQuery(this).html(strtoadd);
-      }
-    });
-    jQuery(document).on("click", ".read-more,.read-less", function () {
-      jQuery(this)
-        .closest(".add-read-more")
-        .toggleClass("show-less-content show-more-content");
-    });
-  }
-
-  AddReadMore();
-});
-
-jQuery(document).ready(function () {
-  jQuery(".show-more").click(function () {
-    if (jQuery(".full-dec").is(":visible")) {
-      jQuery(".short").show();
-      jQuery(".full-dec").hide();
-      jQuery(".show-more").text("read more");
-    } else {
-      jQuery(".short").hide();
-      jQuery(".full-dec").show();
-      jQuery(".show-more").text("read less");
-    }
-  });
+$('.article').readmore({
+  speed: 100,
+  collapsedHeight: 80,
+  lessLink: '<span class="readmore">Read less</span>'
 });
 
 // SlimSelect
