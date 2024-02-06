5b3aa5c36b7a24cc035f4c0f771081a97a424076
sanjaykumbhar26 <sanjay.webidea@gmail.com>
2024-02-06 18:10:28 +0530

    select2

---
+++ src/js/address.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    jQuery('.select-state').select2({
+        dropdownCssClass: 'state-widget ui-state',
+    });
+    jQuery('.select-country').select2({
+        dropdownCssClass: 'state-widget ui-state',
+    });
+});
